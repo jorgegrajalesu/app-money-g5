0198138d03ff83891417a4cd6fc5a40acc2f2761
jorgegrajalesu <jegosystem@gmail.com>
2023-07-26 20:34:30 -0500

    260723

--- js/conversion.js
+++ js/conversion.js
@@ -3,3 +3,11 @@ let rate1 = document.querySelector('.rate1');
 let rate2 = document.querySelector('.rate2');
 let resultBtn = document.querySelector('.result');
 let selects = document.querySelectorAll('.options select');
+let sel1 = selects[0];
+let sel2 = selects[1];
+let inputs = document.querySelectorAll('.input input');
+let inpt1 = inputs[0];
+let inpt2 = inputs[1];
+
+let rates ={};
+let resquestUrl = "https://api.exchangerate.host/latest?base=USD";
